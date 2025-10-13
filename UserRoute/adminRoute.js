3ce27dcbd147cfc16d238a14fb18b813356459e7
Sandesh RaI <pumasandeshrai@gmail.com>
2025-10-13 19:42:39 +0545

    Update adminRoute.js

--- UserRoute/adminRoute.js
+++ UserRoute/adminRoute.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const {Userdetails,AdminPanel,AdminChecker, AdminLogout} = require('../Controller/AdminController')
+const {Userdetails,AdminPanel,AdminChecker} = require('../Controller/AdminController')
 const adminMiddleware = require('../middleware/Middleware')
 const Router = express.Router()
 
 Router.post('/adminlogin',AdminPanel)
 Router.get('/userdata',Userdetails)
 Router.get('/check',adminMiddleware,AdminChecker)
-Router.get('/logout',AdminLogout)
 
 module.exports = Router
