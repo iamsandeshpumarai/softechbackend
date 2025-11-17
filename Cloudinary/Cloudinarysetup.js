24c9c089445733143e81b99d2bddd770b78a2df3
Sandesh RaI <pumasandeshrai@gmail.com>
2025-11-17 21:53:58 +0545

    removed the secret keys and apiname

--- Cloudinary/Cloudinarysetup.js
+++ Cloudinary/Cloudinarysetup.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name:"dcagqmcgc",
-  api_key: "599254344516812",
-  api_secret: "DsCrUTLz0lMDcUC_rswN40g6xBA",
+  cloud_name:process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret:process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
